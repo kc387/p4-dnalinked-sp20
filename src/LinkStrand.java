9e4d5972cab0e803786ab3b55a4e77182247dfb1
Kathleen Chen <kathleen.chen686@duke.edu>
2020-03-23 10:58:08 -0400

    LinkStrand State, Constructors and initialize method

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -4,6 +4,20 @@ authors: Kathleen Chen & Samy Boutouis
 
 public class LinkStrand implements IDnaStrand {
 
+    private class Node {
+        String info;
+        Node next;
+
+        public Node (String s, Node n) {
+            info = s;
+            next = n;
+        }
+    }
+
+    private Node myFirst, myLast;
+    private long mySize;
+    private int myAppends;
+
     @Override
     public long size() {
         return 0;
